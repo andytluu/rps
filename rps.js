72f7d437438a3e4fe7c002537c97d9ca939ba2c1
Andy Luue <155850018+andytluu@users.noreply.github.com>
2024-06-04 19:10:12 -0700

    Added UI

--- rps.js
+++ rps.js
@@ -26,60 +26,69 @@ function playRound(humanChoice, compChoice){
     let computerWin = 0;
     if (hChoice == "rock"){
         if(compChoice == "paper"){
-            console.log("You lose! Paper beats rock");
+            message.textContent = "You lose! Paper beats rock";
+            //console.log("You lose! Paper beats rock");
             return computerWin = 1;
         }
         else if(compChoice == "scissors"){
-            console.log("You win! Rock beats scissors");
+            message.textContent = "You win! Rock beats scissors";
+            //console.log("You win! Rock beats scissors");
             return computerWin= 0;
         }
         else{
-            console.log("It is a tie!");
+            message.textContent = "It is a tie!";
+            //console.log("It is a tie!");
             return computerWin = 2;
         }
     }
 
     if (hChoice == "paper"){
         if(compChoice == "scissors"){
-            console.log("You lose! Scissors beats paper");
+            message.textContent = "You lose! Scissors beats paper";
+            //console.log("You lose! Scissors beats paper");
             return computerWin = 1;
         }
         else if(compChoice == "rock"){
-            console.log("You win! Paper beats rock");
+            message.textContent = "You win! Paper beats rock";
+            //console.log("You win! Paper beats rock");
             return computerWin = 0;
         }
         else{
-            console.log("It is a tie!");
+            message.textContent = "It is a tie!";
+            //console.log("It is a tie!");
             return computerWin = 2;
         }
     }
 
     if (hChoice == "scissors"){
         if(compChoice == "rock"){
-            console.log("You lose! Rock beats scissors");
+            message.textContent = "You lose! Rock beats scissors";
+           // console.log("You lose! Rock beats scissors");
             return computerWin = 1;
         }
         else if(compChoice == "paper"){
-            console.log("You win! Scissors beats paper");
+            message.textContent = "You win! Scissors beats paper";
+           // console.log("You win! Scissors beats paper");
             return computerWin = 0;
         }
         else{
-            console.log("It is a tie!");
+            message.textContent = "It is a tie!";
+           // console.log("It is a tie!");
             return computerWin = 2;
         }
     }
 }
 
-function playGame(){
+function playGame(score){
     let humanScore = 0;
     let compScore = 0;
 
-    for(let i = 0; i<5;i++){
-        const hSelect = getHumanChoice();
-        const cSelect = getComputerChoice();
-        console.log(hSelect);
-        console.log(cSelect);
-        let score = playRound(hSelect,cSelect,humanScore,compScore);
+    //for(let i = 0; i<5;i++){
+        //const hSelect = getHumanChoice();
+        //const cSelect = getComputerChoice();
+        //console.log(hSelect);
+        //console.log(cSelect);
+        //let score = playRound(hSelect,cSelect);
         if(score == 1){
             compScore += 1;
         }
@@ -88,7 +97,66 @@ function playGame(){
         }
         console.log(humanScore);
         console.log(compScore);
-    }
+    //}
 }
 
-playGame();
+function hOrCWin(humanScore,computerScore){
+    if(computerScore === 5){
+        hScore = 0;
+        cScore = 0;
+        return "Computer wins";
+    }
+
+    if(humanScore === 5){
+        hScore = 0;
+        cScore = 0;
+        return "You win";
+    }
+}
+let options = document.querySelector("#options");
+let result = document.querySelector("#results");
+let humanScore = document.querySelector("#human");
+let computerScore = document.querySelector("#computer");
+let winResult = document.querySelector("#win");
+let message = document.querySelector("#message");
+options.addEventListener("click",(event) => {
+    let target = event.target;
+    let score = 0;
+    switch(target.id){
+        case "rock":
+            console.log("rock");
+            score = playRound("rock",getComputerChoice());
+
+            break;
+        case "paper":
+            console.log("paper");
+            score = playRound("paper",getComputerChoice());
+
+            break;
+        case "scissors":
+            console.log("scissors");
+            score = playRound("scissors",getComputerChoice());
+
+            break;
+    }
+
+    if(score == 1){
+        cScore += 1;
+    }
+    else if(score == 0){
+        hScore += 1;
+    }
+    
+    humanScore.textContent = "Your Score: " + hScore.toString();
+    computerScore.textContent = "Computer Score: " + cScore.toString();
+    let win = hOrCWin(hScore,cScore);
+    winResult.textContent = win;
+
+});
+let hScore = 0;
+let cScore = 0;
+/*
+rock.addEventListener("click",playRound("rock"));
+paper.addEventListener("click",playRound("paper"));
+scissors.addEventListener("click",playRound("scissors"));*/
+//playGame();
